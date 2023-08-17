c0b17aae04e73c44939957e0c425cfdc87fa8dce
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-17 12:21:00 +0530

    [Prasanna] Add: Implement the Exception FIle in the Census Data

---
+++ StateCensusAnalyser/StateCensusAnalyser/CensusAnalysisException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateCensusAnalyser
+{
+    public class CensusAnalysisException : Exception
+    {
+        public enum ExceptionType
+        {
+            FILE_INCORRECT,FILE_NOT_EXISTS,HEADER_INCORRECT
+        }
+        public ExceptionType type;
+        public CensusAnalysisException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
--- StateCensusAnalyser/StateCensusAnalyser/StateCensus.cs
+++ StateCensusAnalyser/StateCensusAnalyser/StateCensus.cs
@@ -13,6 +13,18 @@ namespace StateCensusAnalyser
     {
         public static int ReadStateCensusData(string filepath)
         {
+            if(!File.Exists(filepath))
+            {
+                throw new CensusAnalysisException(CensusAnalysisException.ExceptionType.FILE_NOT_EXISTS, "File Not Exists");
+            }
+            if(!Path.GetExtension(filepath).Equals(".csv"))
+            {
+                throw new CensusAnalysisException(CensusAnalysisException.ExceptionType.FILE_INCORRECT, "File Extension incorrect");
+            }
+            if (!File.ReadAllLines(filepath)[0].Equals("State,Population,AreaInSqKm,DensityPerSqKm"))
+            {
+                throw new CensusAnalysisException(CensusAnalysisException.ExceptionType.HEADER_INCORRECT, "Header Incorrect");
+            }
             using (var reader = new StreamReader(filepath))
             {
                 using(var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
