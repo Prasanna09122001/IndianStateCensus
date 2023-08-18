e91931c0f47f4a78cdc50dbab801e86c5a1e5aeb
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-18 10:38:04 +0530

    [Prasanna] Add:Implement the State Code Data Exceptions

--- StateCensusAnalyser/IndianStateCensusDataTest/UnitTest1.cs
+++ StateCensusAnalyser/IndianStateCensusDataTest/UnitTest1.cs
@@ -9,6 +9,12 @@ namespace IndianStateCensusDataTest
         public string stateCensusDataFilepath2= @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCensusDat.csv";
         public string stateCensusDataFilepath3 = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCensus.csv";
         public string stateCensusDataFilePath4 = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCencsusDelimeter.csv";
+
+        public string StateCodeDataFilepath = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCode.csv";
+        public string StateCodeDataFilepath1 = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCode.txt";
+        public string StateCodeDataFilepath2 = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCod.csv";
+        public string StateCodeDataFilepath3 = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCodeH.csv";
+        public string StateCodeDataFilepath4 = @"D:\Bridgelabz Statement\IndianStatesCensus\IndianStateCensus\StateCensusAnalyser\StateCensusAnalyser\File\StateCodeDelimeter.csv";
         [Test]
         public void GivenStateCensusData_WhenAnalysed_RecordsShouldBeMatched()
         {
@@ -56,6 +62,61 @@ namespace IndianStateCensusDataTest
             try
             {
                 StateCensus.ReadStateCensusData(stateCensusDataFilePath4);
+            }
+            catch (CensusAnalysisException ex)
+            {
+                Assert.AreEqual(ex.Message, "Delimeter Incorrect");
+            }
+        }
+
+        //State Code Data
+        [Test]
+        public void GivenStateCodeData_WhenAnalysed_RecordsShouldBeMatched()
+        {
+            Assert.AreEqual(StateCodeCensus.ReadStateCodeData(StateCodeDataFilepath), 37);
+        }
+        [Test]
+        public void GivenStateCodeData_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeCensus.ReadStateCodeData(StateCodeDataFilepath1);
+            }
+            catch (CensusAnalysisException ex)
+            {
+                Assert.AreEqual(ex.Message, "File Extension incorrect");
+            }
+        }
+        [Test]
+        public void GivenStateCodeNoData_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeCensus.ReadStateCodeData(StateCodeDataFilepath2);
+            }
+            catch (CensusAnalysisException ex)
+            {
+                Assert.AreEqual(ex.Message, "File Not Exists");
+            }
+        }
+        [Test]
+        public void GivenStateCodeHeaderIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeCensus.ReadStateCodeData(StateCodeDataFilepath3);
+            }
+            catch (CensusAnalysisException ex)
+            {
+                Assert.AreEqual(ex.Message, "Header Incorrect");
+            }
+        }
+        [Test]
+        public void GivenStateCodeDelimeterIncorrect_WhenAnalysed_ShouldReturnException()
+        {
+            try
+            {
+                StateCodeCensus.ReadStateCodeData(StateCodeDataFilepath4);
             }
             catch (CensusAnalysisException ex)
             {
